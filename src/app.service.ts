2d612c70a2c124df4fb7b45e9834ccd122364a5a
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-03-12 13:43:35 +0900

    [update] install required packages

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello WoAAAAAAAAAAABBBBBBBBBAAAAB!';
+    return 'Hello WoAA!';
   }
 }
